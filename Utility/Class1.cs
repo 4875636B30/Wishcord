b9f654a36c4474a23c0915bcc373cf6730b7d38a
I want it that <goofyahh@gmx.bing>
2023-10-05 22:18:34 +0200

    Joa halt ballin

---
+++ Utility/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utility
+{
+    public class IPAdress
+    {
+        public static string GetIPv4Adress() {
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach(var ip in host.AddressList) {
+                if(!ip.ToString().Contains(':'))
+                    return ip.ToString();
+            }
+            return null;
+        }
+    }
+}
